b1efd6fc9b9cd36d4a7f89b2b593c48c19dda682
81期_弥富 悠佑 <81271@toho.ed.jp>
2026-02-05 23:51:50 +0900

    Force deploy: ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ⚠ ビルド時の型エラーを無視して公開を優先する設定
+    // ビルド時の型エラーを無視して公開を優先する
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ⚠ ビルド時の静的解析エラーを無視する設定
+    // ビルド時のチェック（eslint）を無視する
     ignoreDuringBuilds: true,
   },
 };
